9a733aa4f69c9768c58c75d8c6f79a2f397b67c1
Leigh Fonseca <leigh.fonseca@gmail.com>
2023-04-09 21:23:14 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
